176b67b867853ac4220ae8a03a8f84e27bf6f18c
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-11-10 18:46:37 -0300

    feat: Conectando ao login fxml e adicionando a logo ao diretorio images.

--- src/main/java/br/com/epdmcorp/MainApp.java
+++ src/main/java/br/com/epdmcorp/MainApp.java
@@ -1,8 +1,9 @@
 package br.com.epdmcorp;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,15 +11,16 @@ import java.io.IOException;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        BorderPane root = new BorderPane();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/com/epdmcorp/login.fxml"));
+        Parent root = loader.load();
+
         Scene scene = new Scene(root, 1280, 720);
 
-
-
-        stage.setTitle("Hello!");
+        stage.setTitle("Movimenta");
         stage.setScene(scene);
         stage.show();
     }
+
     public static void main(String[] args) {
         launch();
     }
